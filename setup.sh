8786d7900bce8538b8cd6ad42b63a3ac2659c4b7
MAHIR TAHSAN <106377151+Mahironfire@users.noreply.github.com>
2022-10-11 08:17:14 +0600

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,53 @@
+pip install lolcat
+echo '
+       __Hey guys :)
+   _  |@@|             
+  / \ \--/ __
+  ) O|----|  |   __
+ / / \ }{ /\ )_ / _\
+ )/  /\__/\ \__O (__ This is Mahir
+|/  (--/\--)    \__/
+/   _)(  )(_
+   ----------'| lolcat
+echo " "
+echo "     Scrip By {DoD-99}...!"| lolcat
+echo " "
+apt-get update -y
+apt-get upgrade -y
+apt install python -y
+apt install python2 -y
+apt install python3 -y
+apt install nano -y
+apt install cmatrix -y
+apt install cat -y
+apt install curl -y
+apt install perl -y
+apt install htrrack -y
+apt install openssl -y
+apt install android -y
+apt install toilet -y
+apt install cowsay -y
+apt install git -y
+pkg install figlet -y
+pkg install proot -y
+pkg install screen -y
+pkg install tar -y
+pkg install toilet -y
+apt install wget -y
+apt install termux-tools -y
+apt install pip -y
+apt install pip2 -y
+apt install ruby -y
+apt install php -y
+pip2 install php -y
+pip2 install requests -y
+pip install mechanize -y
+pip2 install mechanize -y
+pip install lolcat -y
+pip install bs4 -y
+pip2 install bs4 -y
+termux-setup-storage
+clear
+echo " TERMUX SET-UP DONE "| lolcat
+echo " "
+echo " NOW YOU CAN USE IT EASILY "| lolcat
